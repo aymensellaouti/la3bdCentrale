cbc54b989520e6528900c13f869b8dd1478b74b5
aymen <aymen.sellaouti@gmail.com>
2019-11-25 09:11:57 +0100

    exercice todo

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -32,4 +32,115 @@ class TodoController extends AbstractController
             'controller_name' => 'TodoController',
         ]);
     }
+
+    /**
+     * @Route("/todo/add/{name}/{content}", name="todo.add")
+     */
+    public function add(Request $request, $name, $content) {
+        /*
+         * Si la session existe
+         *  si non
+         *      message erreur
+         *
+         *  si oui
+         *      si le name existe
+         *          erreur flashbag
+         *
+         *      sinon
+         *          ajoute
+         *          succes
+         *  finsi
+         *  forward
+         */
+        $session = $request->getSession();
+        // si la session contient tableau de todo
+        // si non
+        if(!$session->has('todos')){
+            $session->getFlashBag()->add('error', 'session innexistante');
+        } else {
+            $todos = $session->get('todos');
+            if (isset($todos[$name])) {
+                $session->getFlashBag()->add('error', "$name existe déjà");
+            } else {
+                $todos[$name] = $content;
+                $session->set('todos',$todos);
+                $session->getFlashBag()->add('success', "$name ajouté avec succès");
+            }
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+    /**
+     * @Route("/todo/update/{name}/{content}", name="todo.update")
+     */
+    public function update(Request $request, $name, $content) {
+        /*
+         * Si la session existe
+         *  si non
+         *      message erreur
+         *
+         *  si oui
+         *      si le name n'existe pas
+         *          erreur flashbag
+         *
+         *      sinon
+         *          met a jour
+         *          succes
+         *  finsi
+         *  forward
+         */
+        $session = $request->getSession();
+        // si la session contient tableau de todo
+        // si non
+        if(!$session->has('todos')){
+            $session->getFlashBag()->add('error', 'session innexistante');
+        } else {
+            $todos = $session->get('todos');
+            if (!isset($todos[$name])) {
+                $session->getFlashBag()->add('error', "$name n'existe pas");
+            } else {
+                $todos[$name] = $content;
+                $session->set('todos',$todos);
+                $session->getFlashBag()->add('success', "$name mis à jour avec succès");
+            }
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+    /**
+     * @Route("/todo/delete/{name}", name="todo.delete")
+     */
+    public function delete(Request $request, $name) {
+        /*
+         * Si la session existe
+         *  si non
+         *      message erreur
+         *
+         *  si oui
+         *      si le name n'existe pas
+         *          erreur flashbag
+         *
+         *      sinon
+         *          delete
+         *          succes
+         *  finsi
+         *  forward
+         */
+        $session = $request->getSession();
+        // si la session contient tableau de todo
+        // si non
+        if(!$session->has('todos')){
+            $session->getFlashBag()->add('error', 'session innexistante');
+        } else {
+            $todos = $session->get('todos');
+            if (!isset($todos[$name])) {
+                $session->getFlashBag()->add('error', "$name n'existe pas");
+            } else {
+                unset($todos[$name]);
+                $session->set('todos',$todos);
+                $session->getFlashBag()->add('success', "$name supprimé avec succès");
+            }
+        }
+        return $this->redirectToRoute('todo');
+    }
 }
